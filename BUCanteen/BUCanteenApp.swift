0c93abcebb9b0b90f41c789941c2e886cae14e0d
Prince <yourgitemail@example.com>
2024-09-24 11:10:43 +0530

    Initial Commit

---
+++ BUCanteen/BUCanteenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BUCanteenApp.swift
+//  BUCanteen
+//
+//  Created by Prince on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BUCanteenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
